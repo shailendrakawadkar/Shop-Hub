16b6ac6ef7a3af73b642261ef2abde23623d9846
Shailendra Kawadkar <44092499+shailendrakawadkar@users.noreply.github.com>
2023-10-22 22:06:13 +0530

    Add files via upload

---
+++ src/app/api/auth/nextauth/options.ts
@@ -0,0 +1,38 @@
+import type { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const options: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "Enter your username",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Enter your password",
+        },
+      },
+      async authorize(credentials) {
+        const user = { id: "1001", name: "Shailendra", password: "Kawadkar" };
+        if (
+          credentials?.username === user.name &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+};
